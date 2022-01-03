cf69ee280439d5cd8ac00de51bf98f35ccc8e16f
vic778 <victoiremmanuelbarh@gmail.com>
2022-01-03 16:08:56 +0200

    Imported show function to display in render

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,2 +1,6 @@
-// eslint-disable-next-line no-console
-console.log('hello world');
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import '../scss/style.css';
+import { showsList } from './Display/cards.js';
+
+showsList.renderCards();
